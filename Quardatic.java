418f756686f630000ec79e4ecf5369aac13c85d5
MOHAMMEDZAMEER <zameer4939@gmail.com>
2021-11-09 00:17:15 -0800

    Quardatic.java

---
+++ Quardatic.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class  Quardatic {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print(" Please enter the value of a: ");
+		int a = scan.nextInt();
+		
+		System.out.print(" Please enter the value of b: ");
+		int b = scan.nextInt();
+		
+		System.out.print(" Please enter the value of c: ");
+		int c = scan.nextInt();
+		
+		int delta = b*b - 4*a*c;
+		double x1 = (-b + Math.sqrt(delta)) / 2*a;
+		double x2 = (-b - Math.sqrt(delta)) / 2*a;
+		
+		System.out.println(" The two roots are: " + x1 + " and " + x2);
+		
+	}
+}
