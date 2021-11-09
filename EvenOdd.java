ce7f702f880590ab1bb1ef31e326c9a8f0008603
MOHAMMEDZAMEER <zameer4939@gmail.com>
2021-11-08 21:32:50 -0800

    Even Odd

---
+++ EvenOdd.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EvenOdd{
+
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int n;
+System.out.println("Enter the Number");
+n=sc.nextInt();
+if(n%2==0){
+System.out.println("Number is Even");
+}
+else{
+System.out.println("Number is Odd");
+}
+}
+}
